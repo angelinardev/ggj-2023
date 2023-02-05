b6c4bbfab49173efcc7c40daec2a526838ff4548
Aayush Shrestha <70073615+Hawtori@users.noreply.github.com>
2023-02-05 14:58:53 -0500

    Leaderboard

--- global game jam 2023/Assets/Scripts/Menu/Endbuttons.cs
+++ global game jam 2023/Assets/Scripts/Menu/Endbuttons.cs
@@ -2,9 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using MapDS;
+using System.IO;
+using TMPro;
 
 public class Endbuttons : MonoBehaviour
 {
+    public TMP_Text score;
+
+    private string date;
+    private float time;
+
+    private StreamReader reader;
+    private string path;
+    private List<Map> leaderboard;
+
+    private StreamWriter writer;
+
+    private void Start()
+    {
+        leaderboard = new List<Map>();
+        path = Application.persistentDataPath + "\\scores.txt";
+
+        if (PlayerPrefs.HasKey("Score"))
+        {
+            date = PlayerPrefs.GetString("Date");
+            time = PlayerPrefs.GetFloat("Score");
+        }
+        else time = float.MaxValue;
+        score.text = "Your score:\n" + time.ToString();
+
+        reader = new StreamReader(path);
+
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            string date = line.Split(',')[0];
+            float score = float.Parse(line.Split(',')[1]);
+            leaderboard.Add(new Map(date, score));
+        }
+
+        reader.Close();
+
+        Map temp = new Map("0", 0);
+        leaderboard.Add(new Map(date, time));
+
+        //bubble sort it
+        for(int o = 0; o <= leaderboard.Count - 2; o++)
+        {
+            for(int i = 0; i < o; i++)
+            {
+                if (leaderboard[i].IsGreaterThan(leaderboard[i + 1]))
+                {
+                    temp = leaderboard[i + 1];
+                    leaderboard[i + 1] = leaderboard[i];
+                    leaderboard[i] = temp;
+                }
+            }
+        }
+
+        writer = new StreamWriter(path);
+
+        //save only top 5
+        for(int i = 0; i < leaderboard.Count && i < 6; i++)
+        {
+            writer.WriteLine(leaderboard[i].ToString());
+        }
+
+        writer.Close();
+    }
+
+
 
     public void PlayAgain()
     {
--- global game jam 2023/Assets/Scripts/Menu/MenuButtons.cs
+++ global game jam 2023/Assets/Scripts/Menu/MenuButtons.cs
@@ -4,15 +4,46 @@ using UnityEditor.Purchasing;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+using System.IO;
+using MapDS;
 
 public class MenuButtons : MonoBehaviour
 {
     public Slider sound, music;
 
     public GameObject menu, options, credits;
+    public TMP_Text scores;
 
     private float soundVolume, musicVolume;
 
+    private StreamReader reader;
+    private string path;
+    private List<Map> leaderboard;
+
+    private void Start()
+    {
+        leaderboard = new List<Map>();
+
+        path = Application.persistentDataPath + "\\scores.txt";
+        reader = new StreamReader(path);
+
+        string line;
+        while((line = reader.ReadLine()) != null)
+        {
+            string date = line.Split(',')[0];
+            float score = float.Parse(line.Split(',')[1]);
+            leaderboard.Add(new Map(date, score));
+        }
+
+        scores.text = "Highscores:\n";
+        foreach(Map m in leaderboard)
+        {
+            scores.text += m.ToString() + "\n";
+        }
+
+        reader.Close();
+    }
 
     public void Play()
     {
@@ -58,4 +89,29 @@ public class MenuButtons : MonoBehaviour
         options.SetActive(false);
         credits.SetActive(false);
     }
+}
+
+namespace MapDS
+{
+    public struct Map
+    {
+        private string key;
+        private float value;
+
+        public Map(string index, float info) { key = index; value = info; }
+
+        public Map GetMap() => new Map(key, value);
+        public string GetKey() => key;
+        public float GetValue() => value;
+        public bool IsGreaterThan(float v) => value > v;
+        public bool IsGreaterThan(Map m) => value > m.value;
+        public bool IsLessThan(float v) => value < v;
+        public bool IsLessThan(Map m) => value < m.value;
+        public bool Equals(float v) => value == v;
+        public bool Equals(Map m) => value == m.value;
+
+        public override string ToString() => key + ", " + value;
+
+    }
+
 }
